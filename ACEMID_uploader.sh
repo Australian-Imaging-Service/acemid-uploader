40ab25cd75f64cf0bce9ce055cd237a8b791660f
An Zhao <an.zhao@hotmail.com>
2025-06-26 17:36:47 +0930

    Format the date with yy-mm-dd format.

--- ACEMID_uploader.sh
+++ ACEMID_uploader.sh
@@ -89,8 +89,8 @@ for file in *.db; do
                     # Create a session (experiment) with session type
                     SESSION_TYPE="xnat:xcSessionData"  # Replace with the correct session type
                     # Check if the SESSION_LABEL is numeric
-                    if  [[ "$SESSION_LABEL" =~ ^[0-9]+$ ]]; then
-                        FORMATTED_DATE=$SESSION_LABEL
+                    if  [[ "$SESSION_LABEL" =~ ^[0-9]{14}$ ]]; then
+                        FORMATTED_DATE=$(date -d "${SESSION_LABEL:0:8}" +%Y-%m-%d)                        
                         echo "folder name is in numeric format and insert the date stamp."
                         RESPONSE=$(curl --cookie JSESSIONID=$JS_ID -X PUT "$XNAT_URL/data/archive/projects/$PROJECT_ID/subjects/$SUBJECT_ID/experiments/$SESSION_ID?xsiType=$SESSION_TYPE&label=${SESSION_LABEL}_single_zip&date=$FORMATTED_DATE" -H "Content-Type: application/json" -H "Content-Length: 0" -w "%{http_code}" -o /dev/null)
                         RESPONSE=$(curl --cookie JSESSIONID=$JS_ID -X PUT "$XNAT_URL/data/archive/projects/$PROJECT_ID/subjects/$SUBJECT_ID/experiments/$SESSION_ID?xsiType=$SESSION_TYPE&label=${SESSION_LABEL}_loose_files&date=$FORMATTED_DATE" -H "Content-Type: application/json" -H "Content-Length: 0" -w "%{http_code}" -o /dev/null)
