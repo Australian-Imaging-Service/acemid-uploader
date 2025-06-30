e8b0c3d3a91d135321d25ac457e7c770c71ed67b
An Zhao <an.zhao@hotmail.com>
2025-06-30 15:26:31 +0930

    Update stage_server_monitor.sh

--- stage_server_monitor.sh
+++ stage_server_monitor.sh
@@ -6,10 +6,19 @@ WATCH_DIR="/vol/efs"
 # Log file
 LOG_FILE="/var/log/stage_server.log"
 
+# ACEMID upload script to be called
+ACEMID_UPLOAD_SCRIPT="./ACEMID_data_uploader.sh"
+
 # In this script, we use inotify-tools to monitor changes to files and directories in real time
 # Check if inotifywait is installed or not
 if ! command -v inotifywait &> /dev/null; then
     echo "Error: inotifywait is not installed. Please install inotify-tools."
+    exit 1
+fi
+
+# Check if the ACEMID upload script is exists
+if [ ! -x "ACEMID_UPLOAD_SCRIPT" ]; then
+    echo "Error: ACEMID Upload script is not found at $ACEMID_UPLOAD_SCRIPT"
     exit 1
 fi
 
@@ -21,9 +30,12 @@ inotifywait -m -e create --format '%w%f' "$WATCH_DIR" | while read NEW_ENTRY
 do
     if [ -d "$NEW_ENTRY" ]; then
         echo "$(date '+%Y-%m-%d %H:%M:%S') - New directory detected: $NEW_ENTRY" | tee -a "$LOG_FILE"
+        "$ACEMID_UPLOAD_SCRIPT" "$NEW_ENTRY"  >> $LOG_FILE 2>&1
     elif [ -f "$NEW_ENTRY" ]; then
         echo "$(date '+%Y-%m-%d %H:%M:%S') - New file detected: $NEW_ENTRY" | tee -a "$LOG_FILE"
+        "$ACEMID_UPLOAD_SCRIPT" "$NEW_ENTRY"  >> $LOG_FILE 2>&1
     else
         echo "$(date '+%Y-%m-%d %H:%M:%S') - New item detected (unknown type): $NEW_ENTRY" | tee -a "$LOG_FILE"
+        "$ACEMID_UPLOAD_SCRIPT" "$NEW_ENTRY"  >> $LOG_FILE 2>&1
     fi
 done
